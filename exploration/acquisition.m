42e59ee2e28b167a8839e26ad4cfbf876765550d
rafael <rafael.ellercruz@supelec.fr>
2018-01-17 10:30:16 +0100

    Corrected puissancelisse.m (abs(Z1) was missing in line 43)

--- exploration/acquisition.m
+++ exploration/acquisition.m
@@ -3,8 +3,8 @@ close all;
 % addpath(genpath('C:\Users\daviet_mat\TL-Approf.-BCI\obj'));
 % addpath(genpath('C:\Users\daviet_mat\TL-Approf.-BCI\src'));
 
-% addpath(genpath('/Users/Rafael/sir/TL-Approf.-BCI/obj'));
-% addpath(genpath('/Users/Rafael/sir/'));
+addpath(genpath('/Users/Rafael/sir/TL-Approf.-BCI/obj'));
+addpath(genpath('/Users/Rafael/sir/'));
 
 % addpath(genpath('/Users/heloisehuyghuesdespointes/Documents/TL-Approf.-BCI'));
 
@@ -30,7 +30,7 @@ idx_3 = X(:,2)==3;
 [Y1] = filtre (X,G,delta_f,fe,fc_1);
 [Y2] = filtre (X,G,delta_f,fe,fc_2);
 [Y3] = filtre (X,G,delta_f,fe,fc_3);
-
+%%
 %filters the singal in with fcut ~ 7 Hz. To allow to get only the value
 %corresponding to the amplitude
 windowSize = 36; 
@@ -43,33 +43,28 @@ Y1 = filter(b,a,abs(Y1));
 Y2 = filter(b,a,abs(Y2));
 Y3 = filter(b,a,abs(Y3));
 
-k =10;
-[d1] = derivee(fe,k,Y1);
-[d2] = derivee(fe,k,Y2);
-[d3] = derivee(fe,k,Y3);
 
 
-
-% ax1 = subplot(3,1,1);
-% % figure
-% plot(T(idx_0),Y1(idx_0,1),'r',T(idx_1),Y1(idx_1,1),'b',T(idx_2),Y1(idx_2,1),'y',T(idx_3),Y1(idx_3,1),'k');
-% ax2 = subplot(3,1,2);
-% % figure
-% plot(T(idx_0),Y2(idx_0,1),'r',T(idx_1),Y2(idx_1,1),'b',T(idx_2),Y2(idx_2,1),'y',T(idx_3),Y2(idx_3,1),'k');
-% ax3 = subplot(3,1,3);
-% % figure
-% plot(T(idx_0),Y3(idx_0,1),'r',T(idx_1),Y3(idx_1,1),'b',T(idx_2),Y3(idx_2,1),'y',T(idx_3),Y3(idx_3,1),'k');
-
-% %Affichage de la dérivée de Y1, Y2, Y3 
 ax1 = subplot(3,1,1);
 % figure
-plot(T(idx_0),d1(idx_0,1),'r',T(idx_1),d1(idx_1,1),'b',T(idx_2),d1(idx_2,1),'y',T(idx_3),d1(idx_3,1),'k');
+plot(T(idx_0),Y1(idx_0,1),'r',T(idx_1),Y1(idx_1,1),'b',T(idx_2),Y1(idx_2,1),'y',T(idx_3),Y1(idx_3,1),'k');
 ax2 = subplot(3,1,2);
 % figure
-plot(T(idx_0),d2(idx_0,1),'r',T(idx_1),d2(idx_1,1),'b',T(idx_2),d2(idx_2,1),'y',T(idx_3),d2(idx_3,1),'k');
+plot(T(idx_0),Y2(idx_0,1),'r',T(idx_1),Y2(idx_1,1),'b',T(idx_2),Y2(idx_2,1),'y',T(idx_3),Y2(idx_3,1),'k');
 ax3 = subplot(3,1,3);
 % figure
-plot(T(idx_0),d3(idx_0,1),'r',T(idx_1),d3(idx_1,1),'b',T(idx_2),d3(idx_2,1),'y',T(idx_3),d3(idx_3,1),'k');
+plot(T(idx_0),Y3(idx_0,1),'r',T(idx_1),Y3(idx_1,1),'b',T(idx_2),Y3(idx_2,1),'y',T(idx_3),Y3(idx_3,1),'k');
+
+% % %Affichage de la dérivée de Y1, Y2, Y3 
+% ax1 = subplot(3,1,1);
+% % figure
+% plot(T(idx_0),d1(idx_0,1),'r',T(idx_1),d1(idx_1,1),'b',T(idx_2),d1(idx_2,1),'y',T(idx_3),d1(idx_3,1),'k');
+% ax2 = subplot(3,1,2);
+% % figure
+% plot(T(idx_0),d2(idx_0,1),'r',T(idx_1),d2(idx_1,1),'b',T(idx_2),d2(idx_2,1),'y',T(idx_3),d2(idx_3,1),'k');
+% ax3 = subplot(3,1,3);
+% % figure
+% plot(T(idx_0),d3(idx_0,1),'r',T(idx_1),d3(idx_1,1),'b',T(idx_2),d3(idx_2,1),'y',T(idx_3),d3(idx_3,1),'k');
 
 %Lines of reference.
 refline(ax1,[0 1e-3]);
