63bddc17fba0648ddbee24f79e86335c74d4484b
rafael <rafael.ellercruz@supelec.fr>
2018-01-17 11:07:44 +0100

    Created envelope_extractor, the class that will be used to get the commands in the ROS program

--- src/python/filtre.py
+++ src/python/filtre.py
@@ -1,11 +1,14 @@
 import numpy as np
 from scipy.signal import lfilter
 
-def filtre(X,G,delta_f,fe,fc):
-    a1 = -2*np.sqrt(1-(2*np.pi*G*delta_f)/fe)*np.cos(2*np.pi*fc/fe)
-    a2 = 1-(2*np.pi*G*delta_f)/fe;
-    b0 = (2*np.pi*G*delta_f/fe)*np.sin(2*np.pi*fc/fe);
+
+def filtre(X, G, delta_f, fe, fc):
+    """The filtering function as defined in filtre.m, already validated, works fine.
+    """
+    a1 = -2 * np.sqrt(1 - (2 * np.pi * G * delta_f) / fe) * np.cos(2 * np.pi * fc / fe)
+    a2 = 1 - (2 * np.pi * G * delta_f) / fe
+    b0 = (2 * np.pi * G * delta_f / fe) * np.sin(2 * np.pi * fc / fe)
     b0 = np.array([b0])
-    A = np.array([ 1, a1, a2 ]);
+    A = np.array([1, a1, a2])
     # print('size A = {} \nsize B = {} \nsize X = {}'.format(A.shape,b0.shape,X.shape))
-    return lfilter(b0,A,X);
+    return lfilter(b0, A, X)
