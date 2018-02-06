9f85c96946791019b101440b4b93ac1600f771b3
Rafael <rafael@cscruz.org>
2018-02-06 17:23:35 +0100

    Changed some stuff in the ROS part to make it easier to run and finished commenting. Also solved the z squared problem, the new optimal values calculated by Helo are also set

--- ros_ws/src/bci_robot/scripts/bci_source.py
+++ ros_ws/src/bci_robot/scripts/bci_source.py
@@ -1,33 +1,23 @@
 #!/usr/bin/env python
-# license removed for brevity
 import rospy
-# from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64
 import numpy as np
+import sys
+from os import path
 
-def bci_source():
-    #TODO set default file and default mode as argument when initializing
+
+def bci_source(file_path):
     print("Initializing /bci_source")
     print("""This node feeds a 256 Hz stream of samples from the BCI measurements,
     to a topic called dataBCI. The current version simulates the real-time acquisition
-    of the signal by reading a text file containing real measurements. Please, type the
-    full path of the file:
-    (Ex: '/home/user/TL-Approf.-BCI/obj/herve001.txt')
+    of the signal by reading a text file containing real measurements.
     """)
 
-    #SECTION USED FOR TESTING------------------
-    # file_path = input()
-    file_path = '/home/rafael/dev/sir/TL-Approf.-BCI/obj/herve002_labeled.txt'
-    Labeled = True
-    #UNCOMMENT THE FIRST ONE AND COMMENT THE SECOND AFTERWARDS-------------------------
-
+    #Reads all the information from the file
     X = np.loadtxt(file_path)
-    #Checks if the measures are taken in two different locations, if so, it averages them
-    if len(X.shape)==2 and not Labeled:
-        X = (X[:,0]+X[:,1])/2
-    elif Labeled:
-        lab = X[:,1]
-        X = X[:,0]
+    #Separates the labels from the signal
+    lab = X[:,1]
+    X = X[:,0]
 
     pub = rospy.Publisher('dataBCI', Float64, queue_size=10) #Try other queue_sizes
     rospy.init_node('bci_source', anonymous=True)
@@ -44,8 +34,7 @@ def bci_source():
         #reads and publishes sample for sample at 256 Hz
         sample.data = X[index]
         index+=1
-        if Labeled:
-            print(get_command(lab[index]))
+        print(get_command(lab[index]))
         # print(sample)
         pub.publish(sample)
         rate.sleep()
@@ -53,6 +42,8 @@ def bci_source():
     print("Finished publishing the data from file")
 
 def get_command(num):
+    """This function translates the label used to annotate the signals to the corresponding commands
+    """
     if num == 0:
         return 'Stop'
     elif num == 1:
@@ -61,11 +52,31 @@ def get_command(num):
         return 'Right'
     elif num == 3:
         return 'Left'
-    else:
-        return "your code sucks"
 
 if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        #----------ATTENTION-----------------------
+        #This is a hard-coded section, you're NOT required to use it. It exists to make it
+        #easier and faster to call the node, as it does not require to inform the full path of
+        #the file as argument in the console.
+
+        #TO USE IT substitute the path for the appropriate one in your system.
+        file_path = '/home/rafael/dev/sir/TL-Approf.-BCI/obj/herve002_labeled.txt'
+
+        if path.isfile(file_path):
+            print("Opening file in {}".format(file_path))
+        else:
+            print("Wrong usage: Try informing in the console the full path to the signal file, for example:")
+            print("rosrun bci_robot bci_source.py /home/user/project/obj/herve002_labeled.txt")
+            raise ValueError("{} is not a valid path".format(file_path))
+
+    elif len(sys.argv) == 2:
+        #This is the normal usage of this program, you should inform the full path of the
+        #file containing the signal to be published by this node. The labeled files are
+        #normally in the /obj/ folder of the project.
+        file_path = sys.argv[1]
+
     try:
-        bci_source()
+        bci_source(file_path)
     except rospy.ROSInterruptException:
         pass
--- ros_ws/src/bci_robot/scripts/signal_pro.py
+++ ros_ws/src/bci_robot/scripts/signal_pro.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 import rospy
-# from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64
 from geometry_msgs.msg import Twist
 import numpy as np
 from scipy.ndimage.interpolation import shift
+import sys
+from socket import gethostbyname
 
 class Signal_processer(object):
 
-
-    def __init__(self, G=0.56, delta_f=0.46, alpha=0.99, bufferSize=32, fe = 256,
-                speed = 0.1, aspeed = 0.5, thold = [3.5e-08, 2.1e-08, 1.4e-08]):
-        '''Initializes the signal processor with the optimal parameters. The speed
-        parameters were chosen as to run it safely in a small room.
+    def __init__(self, G=0.56, delta_f=0.46, alpha=0.96, bufferSize=32, fe = 256,
+                speed = 0.1, aspeed = 0.5, thold = [2.9e-06, 1.7e-06, 1.4e-06],mode='turtle'):
+        '''Initializes the signal processor with the optimal and default parameters.
+        The speed parameters were chosen as to run it safely in a small room.
         '''
         self.fe = fe
         self.fcent = [7.5, 11, 13.5]  #The central frequencies of the flashing lights
@@ -22,6 +22,7 @@ class Signal_processer(object):
 
         self.alpha = alpha
         self.N = bufferSize #Stores the bufferSize in a variable N
+
         #Variable that stores the current output of the pass-band filters in position 0
         #and two of its previous values, each column represents a slice of time and each
         #line represents the signal filtered in one of the given frequencies
@@ -48,25 +49,35 @@ class Signal_processer(object):
         #Definition of the 3 Thresholds that will be used to extract the commands
         self.thold = thold
 
-
-        #ROS members
-        rospy.init_node('signal_processer', anonymous=True)
+        #---ROS members---
+        rospy.init_node('signal_processing', anonymous=True)
         self.sub = rospy.Subscriber("dataBCI", Float64, self.callback_data_listen)
-        #TODO set it to String or something else
-        #Is it better to publish directly to the robot or to send it to a different node?
 
-
-        #INFO------------------------------------------------------------------
         #Select the appropriate Topic. The first one corresponds to the turtlesim
         #node available in the standard ROS package. The second is the Khepera bot
-        # self.pub = rospy.Publisher("/turtle1/cmd_vel",Twist, queue_size=10)
-        self.pub = rospy.Publisher("/cmd_vel",Twist, queue_size=10)
+        if mode == 'turtle':
+            self.pub = rospy.Publisher("/turtle1/cmd_vel",Twist, queue_size=10)
+        elif mode == 'khepera':
+            self.pub = rospy.Publisher("/cmd_vel",Twist, queue_size=10)
+        else:
+            print("Incorrect Input: The normal usage of the node is")
+            print("---For testing locally in a turtlesim node---")
+            print("rosrun bci_robot signal_pro.py turtle")
+            print("---For running while connected to a kephera robot---")
+            print("rosrun bci_robot signal_pro.py kephera")
+            raise ValueError("Incorrect call to ROS node")
+
 
         #The adress of the robot is khepera2.smart.metz.supelec.fr or khepera1...
         #Do a ping on it to get the IP address and then to launch the node
         #roslaunch kteams_node khepera_bringup.launch khepera_dns:=THE_IP_OF_THE_ROBOT
 
     def callback_data_listen(self,sample):
+        """This callback corresponds to all the processing that happens once the node
+        receives a message from the BCI source. It treats the samples one by one
+        at the sampling frequency (256 Hz for the stored files) and stores them in
+        a buffer of size defined in the class initialization.
+        """
         #Starts by shifting the columns of y by one position to the right
         self.increment_y()
         #Then apllies the 3 filters to the current sample
@@ -82,8 +93,6 @@ class Signal_processer(object):
             self.callback_pub()
         else:
             self.i+=1
-        #TEST------
-        # self.pub.publish(Float64(data=self.y[0,0]))
 
     def exec_(self):
         """Executes the nodes
@@ -122,7 +131,7 @@ class Signal_processer(object):
         #URGENT: As Mathieu noticed, the second element shouldn't be squared, since it
         #is already a squared z
         for j in [0,1,2]:
-            self.buffer_z[j,self.i] = (1-self.alpha)*(self.y[j,0])**2 + self.alpha*(self.buffer_z[j,(self.i-1)%self.N])**2
+            self.buffer_z[j,self.i] = (1-self.alpha)*(self.y[j,0])**2 + self.alpha*(self.buffer_z[j,(self.i-1)%self.N])
 
     def get_command(self):
         """This method decides from the full z_buffer what the corresponding command
@@ -164,8 +173,17 @@ class Signal_processer(object):
 
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Incorrect Input: The normal usage of the node is")
+        print("---For testing locally in a turtlesim node---")
+        print("rosrun bci_robot signal_pro.py turtle")
+        print("---For running while connected to a kephera robot---")
+        print("rosrun bci_robot signal_pro.py kephera")
+        raise ValueError("Incorrect call to ROS node")
     try:
-        sigpro = Signal_processer()
+        print("Initializing the signal_processing node in mode: {}".format(sys.argv[1]))
+        sigpro = Signal_processer(mode=sys.argv[1])
+        print('Node up and running')
         sigpro.exec_()
     except rospy.ROSInterruptException:
         pass
